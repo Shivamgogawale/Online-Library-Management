4521b0818319b9bf129213e659f4378d9eacd0ed
Shivam <shivam.gogawale@bnt-soft.com>
2023-12-04 18:03:45 +0530

    add controller

---
+++ src/main/java/com/onlineLibrary/controller/OnlineLibraryController.java
@@ -0,0 +1,14 @@
+package com.onlineLibrary.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OnlineLibraryController {
+
+	private static final Logger logger=LoggerFactory.getLogger(OnlineLibraryController.class);
+	
+	
+	
+}
